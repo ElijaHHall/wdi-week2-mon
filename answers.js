2434bc726d20faa3b08781d19727aa5a06cfe03c
ElijahHHall <ehall851@gmail.com>
2018-03-26 19:00:05 -0600

    3rd commit

---
+++ answers.js
@@ -0,0 +1,63 @@
+// 1. For a light switch, the data type would be "label" and then setting a class element to 
+// to a switch.
+
+// 2. The data type for a user's email would be to set it to assign it to a variable.
+// 3. A spaceship with a hull, laser blasters, tractor beam, and warp drive
+// would correspond to an object with an array of those different 
+// components in the ship. 
+
+// 4. A list of student name from our class would be an array.
+// The data structure of this would be set to the data value of the students, and
+// each students name. 
+
+// 1. const rainbowColors = ["Red", "Blue", "Yellow", "Orange", "Purple", "Green"];
+// console.log(rainbowColors[1]);
+
+// 2. const elijah = [
+// 	{faveFood: "Ethiopian"},
+// 	{hobby: "Video Games"},
+// 	{homeTown: "Denver"},
+// 	{faveDatatype: "if"},
+// ];
+
+// console.log(elijah[1].hobby);
+
+// const crazyObject = {
+//   taco: [{meat: 'steak',
+//          cheese: ['panela', 'queso', 'chihuahua']},
+//          {meat: 'chicken',
+//           salsa: ["pico", "hot", "hotter", "really hot", "really really hot", "omg my mouth is burning"]},
+//         ],
+//   larry: {
+//     nicknames: ["LD", "Chicken Teriyaki Boyyyyyy"],
+//     quotes: ["Pretty pretty prettayyyyy good", "Is that a parkinson's thing?", "women love a self confident bald man", "I'm a walking talking enigma"],
+//     characters: [{
+//       name: "Jeff",
+//       occupation: "manager"
+//     },
+//     {
+//       name: "funkhauser",
+//       occupation: "tv dude"
+//     },
+//     {
+//       name: "susie",
+//       occupation: "jeffs wife",
+//       favourtieHobby: "Swearing at Larry and Jeff"
+//     },
+//     ]
+//   }
+// }
+// crazyObject.taco[1].salsa[5]
+// crazyObject.larry.quotes[0]
+// crazyObject.larry.characters[2].favourtieHobby
+// crazyObject.larry.nicknames[1]
+// crazyObject.larry.characters[1].name
+
+
+
+
+
+
+
+
+
